9a6bc7f6f47b6d0e1377f03ab2a2d3c33ee7b945
julianguacaneme <julianguacaneme@gmail.com>
2021-11-11 10:46:29 -0500

    implemento en html

--- descuentos.js
+++ descuentos.js
@@ -17,3 +17,17 @@ function calcularPrecioConDescuento(precio, descuento) {
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
 }
+
+function  onclickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
+    
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    
+    const resultP = document.getElementById("resultP")
+    resultP.innerText = "El precio con descuento son $" + precioConDescuento; 
+    
+}
