ca2415dbea81b21f3ddbd50b9b1ba2f9051299a8
julianguacaneme <julianguacaneme@gmail.com>
2021-11-15 16:47:23 -0500

    salarios

---
+++ salarios.js
@@ -0,0 +1,53 @@
+const colombia = [];
+colombia.push(colombia {
+    name: "Lucia",
+    salary: 2000,
+});
+colombia.push(colombia {
+    name: "Julián",
+    salary: 800,
+});
+colombia.push(colombia {
+    name: "Luis E",
+    salary: 1500,
+});
+colombia.push(colombia {
+    name: "Carito",
+    salary: 850,
+});
+colombia.push(colombia {
+    name: "Diana",
+    salary: 1000,
+});
+colombia.push(colombia {
+    name: "Mauricio",
+    salary: 1300,
+});
+colombia.push(colombia {
+    name: "Hernando",
+    salary: 1800,
+});
+colombia.push(colombia {
+    name: "LuisFer",
+    salary: 2600,
+});
+colombia.push(colombia {
+    name: "Raquel",
+    salary: 1800,
+});
+colombia.push(colombia {
+    name: "Andres",
+    salary: 2800,
+});
+colombia.push(colombia {
+    name: "Alejandra",
+    salary: 1700,
+});
+colombia.push(colombia {
+    name: "Diego",
+    salary: 2100,
+});
+colombia.push(colombia {
+    name: "Diana de P",
+    salary: 2000,
+});
